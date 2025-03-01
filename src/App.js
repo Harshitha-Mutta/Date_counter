8638b6b0cbc7a16d42f3cef46d445e4043f66381
Mutta Harshitha <2000137403@hexaware.com>
2025-03-01 19:16:27 +0530

    second Commit

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ function App() {
   function handleCountMinus(){
     setCount(count-step)
   }
-  
+  function handleReset(){
+    setCount(0)
+    setStep(1);
+  }
   const date=new Date();
   date.setDate(date.getDate()+count);
   return (
     <div>  
       <div className="stepper">    
-          <button className="button" onClick={handleStepMinus}>-</button>
+          {/* <button className="button" onClick={handleStepMinus}>-</button>
           Step:{step}
-          <button className="button" onClick={handleStepPlus}>+</button>
+          <button className="button" onClick={handleStepPlus}>+</button> */}
+          <input type="range" min="0" max="10" value={step} onChange={e=>setStep(Number(e.target.value))}/>{step}
       </div>
       <div className="stepper">
         <button className="button" onClick={handleCountMinus}>-</button>
-        Count:{count}
+        <input type="text" value={count} onChange={e=>(setCount(Number(e.target.value)))}/>
         <button className="button" onClick={()=>{setCount(c=>c+step)}}>+</button>
       </div>
       <div>
@@ -32,7 +36,9 @@ function App() {
         <p>{count>0 &&`${count} days from today is ${date.toDateString()}`}</p>
         <p>{count<0 &&`${count*-1} days ago was ${date.toDateString()}`}</p>
       </div>
-      
+      <div>
+        {(count!==0 || step!==1) ?<button onClick={handleReset}>Reset</button>: null}
+      </div>
     </div>
   );
 }
